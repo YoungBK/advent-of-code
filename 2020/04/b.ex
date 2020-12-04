65214a5f48bbd8797b824a1cecd8782fd0b548d9
Barry Kim <barrykim@gmail.com>
2020-12-03 22:55:17 -0800

    Cleaned up the second answer to the 4th day of 2020

--- 2020/04/b.ex
+++ 2020/04/b.ex
@@ -15,33 +15,32 @@ defmodule AdventOfCode202004.B do
 """    
 pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
 hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
 """ |> String.trim()
     |> String.split(~r/[\n ]/)
   end
 
-
-# eyr:2029 ecl:blu cid:129 byr:1989
-# iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
-# 
-# hcl:#888785
-# hgt:164cm byr:2001 iyr:2015 cid:88
-# pid:545766238 ecl:hzl
-# eyr:2022
-# 
-# iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+  def valid_number?(number, min, max), do: min <= number && number <= max
 
   def valid_year?(yearstr, min, max) do
-    year = String.to_integer(yearstr)
-    min <= year && year <= max
+    String.to_integer(yearstr) |> valid_number?(min, max)
   end
 
-  def valid_height?(height, min, max), do: min <= height && height <= max
   def valid_height?(heightstr) do
     cond do
       String.ends_with?(heightstr, "in") ->
-        String.split(heightstr, "in") |> hd() |> String.to_integer() |> valid_height?(59, 76)
+        String.split(heightstr, "in") |> hd() |> String.to_integer() |> valid_number?(59, 76)
       String.ends_with?(heightstr, "cm") ->
-        String.split(heightstr, "cm") |> hd() |> String.to_integer() |> valid_height?(150, 193)
+        String.split(heightstr, "cm") |> hd() |> String.to_integer() |> valid_number?(150, 193)
       true ->
         false
     end
@@ -49,24 +48,14 @@ hcl:#623a2f
 
   def valid_haircolor?(haircolor) do
     try do
-      color = String.split(haircolor, "#") |> tl() |> hd() |> String.to_integer(16)
-      color >= 0
+      String.split(haircolor, "#") |> tl() |> hd() |> String.to_integer(16) >= 0
     catch
       _, _ -> false
     end
   end
   
   def valid_eyecolor?(eyecolor) do
-    case eyecolor do
-      "amb" -> true
-      "blu" -> true
-      "brn" -> true
-      "gry" -> true
-      "grn" -> true
-      "hzl" -> true
-      "oth" -> true
-      _ -> false
-    end
+    eyecolor in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
   end
 
   def valid_passportid?(pid) do
@@ -77,20 +66,21 @@ hcl:#623a2f
     end
   end
   
-  # cid (Country ID)
+  # cid (Country ID) ignored
   def valid?(%{"byr" => byr, "iyr" => iyr, "eyr" => eyr, "hgt" => hgt, "hcl" => hcl, "ecl" => ecl, "pid" => pid}) do
-    valid_year?(byr, 1920, 2002) && valid_year?(iyr, 2010, 2020) &&
-      valid_year?(eyr, 2020, 2030) && valid_height?(hgt) && valid_haircolor?(hcl) &&
-      valid_eyecolor?(ecl) && valid_passportid?(pid)
+    valid_year?(byr, 1920, 2002) &&
+      valid_year?(iyr, 2010, 2020) &&
+      valid_year?(eyr, 2020, 2030) &&
+      valid_height?(hgt) &&
+      valid_haircolor?(hcl) &&
+      valid_eyecolor?(ecl) &&
+      valid_passportid?(pid)
   end
   def valid?(_), do: false
                
   def add_to_passport(list, passport \\ %{})
   def add_to_passport([], passport), do: {passport, nil}
-  def add_to_passport([key_val | rest] = list, passport) do
-    # IO.inspect(passport, label: "add_to_passport passport")
-    # IO.inspect(list, label: "add_to_passport list")
-    # IO.inspect(key_val, label: "add_to_passport key_val")
+  def add_to_passport([key_val | rest], passport) do
     case key_val do
       "" ->
         {passport, rest}
@@ -101,7 +91,6 @@ hcl:#623a2f
   end
 
   def group(list, retlist \\ []) do
-    # IO.inspect(retlist, label: "group retlist")
     {passport, rest} = add_to_passport(list)
     case rest do
       nil -> [passport | retlist]
@@ -109,15 +98,12 @@ hcl:#623a2f
     end
   end
 
+  def count_valid(list, count \\ 0)
   def count_valid([], count), do: count
-  def count_valid([first | rest], count \\ 0) do
+  def count_valid([first | rest], count) do
     case valid?(first) do
-      true ->
-        # IO.inspect(first, label: "is valid")
-        count_valid(rest, count + 1)
-      false ->
-        # IO.inspect(first, label: "is NOT valid")
-        count_valid(rest, count)
+      true -> count_valid(rest, count + 1)
+      false -> count_valid(rest, count)
     end
   end
 
